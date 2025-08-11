ebc65e2798026a7808b35b610dea206087f46ae3
BATTHINA KRISHNA KOWSHIK <2300031283@kluniversity.in>
2025-08-11 22:39:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>WELCOME TO CICD</h1>
+        <p>Current count: {count}</p>
+        <div>
+          <button onClick={increment}>+</button>
+          <button onClick={decrement} disabled={count === 0}>
+            -
+          </button>
+        </div>
       </header>
     </div>
   );
